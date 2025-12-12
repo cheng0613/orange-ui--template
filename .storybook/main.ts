916b118320495583b4a38c8b6819f6b774f36645
jiancheng <jiancheng@juneyaoair.com>
2025-12-12 14:31:48 +0800

    fix(42408): 修复组件库名称引用的问题;

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,7 @@
 import type { StorybookConfig } from '@storybook/vue3-vite'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+import { mergeConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,20 +13,16 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag'
   },
-  viteFinal: async config => {
-    // 添加路径别名
-    config.resolve = config.resolve || {}
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': resolve(__dirname, '../'),
-      '@ofe/ui-vue': resolve(__dirname, '../packages/ofe-ui'),
-      'element-plus': resolve(
-        __dirname,
-        '../packages/ofe-ui/node_modules/element-plus'
-      )
-    }
-
-    return config
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [vue()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '../'),
+          '@ofe/ui-vue': path.resolve(__dirname, '../packages/ofe-ui/index.ts')
+        }
+      }
+    })
   }
 }
 
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,12 @@
-import type { Preview } from '@storybook/vue3-vite'
+import type { Preview } from '@storybook/vue3'
+import { setup } from '@storybook/vue3'
+import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import '../packages/ofe-ui/dist/style.css'
+
+// 全局 Vue 配置
+setup(app => {
+  app.use(ElementPlus)
+})
 
 const preview: Preview = {
   parameters: {
